f55e444a170c3d6d76ada745bc50ab8850c056c2
JayJayTing <tingjayj@gmail.com>
2019-07-02 13:38:38 -0400

    done

--- typewriter.js
+++ typewriter.js
@@ -1,23 +1,27 @@
 const sentence = "hello there from lighthouse labs";
 
-
-
-
 function repeat(sentence, index)
 {
-    
-    if (index < sentence.length - 1) {
-        setTimeout(() => {
-            process.stdout.write(sentence[index]);
-            repeat(sentence, index+1);
-        }, 100);
-        
-    }else if(index = sentence.length -1){
-        setTimeout(() => {
-            process.stdout.write(sentence[index]);
-            console.log();
-        }, 100);
+
+    if (index >= sentence.length) {
+        console.log();
+        return;
     }
+
+    setTimeout(()=> {process.stdout.write(sentence[index])
+    repeat(sentence, index+1)}, 100);
+
+    // if (index < sentence.length - 1) {
+    //     setTimeout(() => {
+    //         process.stdout.write(sentence[index]);
+    //         repeat(sentence, index+1);
+    //     }, 1000);
+        
+    // }else if(index = sentence.length -1){
+    //     setTimeout(() => {
+    //         process.stdout.write(sentence[index]);
+    //     }, 1000);
+    // }
 
 
 }
